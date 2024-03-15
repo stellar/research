0a550708e6a482d72bf7fd0fa738b75ec0cc3c6b
Giuliano Losa <giuliano@losa.fr>
2024-03-14 20:55:20 -0700

    try to configure for github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,14 +14,17 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://research.stellar.org',
+  // url: 'git@github.com:stellar/research.git',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Stellar', // Usually your GitHub org/user name.
-  projectName: 'sdf-research', // Usually your repo name.
+  organizationName: 'stellar', // Usually your GitHub org/user name.
+  projectName: 'research', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
